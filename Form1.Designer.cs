0257954282a945207952079b57162cda56373ec5
Michael Redman <50535591+mwr0007@users.noreply.github.com>
2022-01-21 00:03:20 -0600

    project setup, basic drawing

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace demoapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.DrawWallButton = new System.Windows.Forms.Button();
+            this.DrawFurnitureButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SplitContainer
+            // 
+            this.SplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SplitContainer.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer.IsSplitterFixed = true;
+            this.SplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.SplitContainer.Name = "SplitContainer";
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.Controls.Add(this.DrawFurnitureButton);
+            this.SplitContainer.Panel1.Controls.Add(this.DrawWallButton);
+            // 
+            // SplitContainer.Panel2
+            // 
+            this.SplitContainer.Panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SplitContainer.Panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SplitContainer_Panel2_MouseDown);
+            this.SplitContainer.Size = new System.Drawing.Size(1250, 577);
+            this.SplitContainer.SplitterDistance = 625;
+            this.SplitContainer.SplitterWidth = 1;
+            this.SplitContainer.TabIndex = 0;
+            // 
+            // DrawWallButton
+            // 
+            this.DrawWallButton.Location = new System.Drawing.Point(495, 11);
+            this.DrawWallButton.Name = "DrawWallButton";
+            this.DrawWallButton.Size = new System.Drawing.Size(115, 29);
+            this.DrawWallButton.TabIndex = 0;
+            this.DrawWallButton.Text = "Draw Wall";
+            this.DrawWallButton.UseVisualStyleBackColor = true;
+            this.DrawWallButton.Click += new System.EventHandler(this.DrawWallButton_Click);
+            // 
+            // DrawFurnitureButton
+            // 
+            this.DrawFurnitureButton.Location = new System.Drawing.Point(495, 46);
+            this.DrawFurnitureButton.Name = "DrawFurnitureButton";
+            this.DrawFurnitureButton.Size = new System.Drawing.Size(115, 29);
+            this.DrawFurnitureButton.TabIndex = 1;
+            this.DrawFurnitureButton.Text = "Draw Furniture";
+            this.DrawFurnitureButton.UseVisualStyleBackColor = true;
+            this.DrawFurnitureButton.Click += new System.EventHandler(this.DrawFurnitureButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 577);
+            this.Controls.Add(this.SplitContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private System.Windows.Forms.Button DrawFurnitureButton;
+        private System.Windows.Forms.Button DrawWallButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demoapp
+{
+    // The GridObject enum gives us a more convenient way to reference the different types of
+    // objects that the user can place on the UI.
+    // Ensure that any additions to this enum correspond to a Brush in RoomBrushTypes.
+    internal enum GridObject
+    {
+        Empty,
+        Wall,
+        Furniture,
+    }
+
+    public partial class Form1 : Form
+    {
+        private Brush ActiveBrush = Brushes.Red;
+        private Panel SimDrawingPanel;
+        private Graphics SimDrawingPanelGraphics;
+        private int PanelXBounds, PanelYBounds, PanelXMin, PanelYMin;
+        private static int SQUARE_SIZE = 25;
+        private static int GRID_DIM = 25;
+
+        // RoomBrushTypes stores the Brushes for drawing various items in the room designer, such as walls, furniture, etc.
+        // The elements in this array should be ordered to match the GridObject enum.
+        private Brush[] RoomBrushTypes = new Brush[] { Brushes.White, Brushes.Red, Brushes.Blue };
+
+        // GridSpacesPositions is a 2d array of Points that stores the top left corner coordinate of each space in the room designer array.
+        private Point[,] GridSpacesPositions = new Point[GRID_DIM, GRID_DIM];
+
+        // GridSpacesPositions keeps track of what element is at what position. To be used in the simulation to represent the room.
+        private GridObject[,] GridSpacesStatus = new GridObject[GRID_DIM, GRID_DIM];
+
+        // Callback for clicking the button "Draw Furniture" button
+        private void DrawFurnitureButton_Click(object sender, EventArgs e)
+        {
+            ActiveBrush = RoomBrushTypes[(int)GridObject.Furniture];
+        }
+
+        // Callback for clicking the button "Draw Wall" button
+        private void DrawWallButton_Click(object sender, EventArgs e)
+        {
+            ActiveBrush = RoomBrushTypes[(int)GridObject.Wall];
+        }
+
+        // Callback for clicking within the drawing window
+        private void SplitContainer_Panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Calculate the grid index of the clicked cell
+            int xidx = (int)Math.Floor(e.X / (double)SQUARE_SIZE);
+            int yidx = (int)Math.Floor(e.Y / (double)SQUARE_SIZE);
+            Point p = GridSpacesPositions[xidx, yidx];
+            // Do the drawing
+            SimDrawingPanelGraphics.FillRectangle(ActiveBrush, p.X, p.Y, SQUARE_SIZE, SQUARE_SIZE);
+
+            // Store object type in
+            GridSpacesStatus[xidx, yidx] = (GridObject)Array.IndexOf(RoomBrushTypes, ActiveBrush);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            SimDrawingPanel = SplitContainer.Panel2;
+            SimDrawingPanelGraphics = SimDrawingPanel.CreateGraphics();
+            PanelXBounds = SimDrawingPanel.Width;
+            PanelYBounds = SimDrawingPanel.Height;
+            PanelXMin = SimDrawingPanel.Width - PanelXBounds;
+            PanelYMin = SimDrawingPanel.Height - PanelYBounds;
+
+            // init grid spaces
+            for (int i = 0; i < SQUARE_SIZE; i++)
+            {
+                for (int j = 0; j < SQUARE_SIZE; j++)
+                {
+                    GridSpacesPositions[j, i] = new Point(j * SQUARE_SIZE, i * SQUARE_SIZE);
+                    GridSpacesStatus[j, i] = GridObject.Empty;
+                }
+            }
+        }
+    }
+}
