62e2cc846ca9b5743ac50634782a5410668bfb4f
Michael Redman <50535591+mwr0007@users.noreply.github.com>
2022-02-04 14:35:38 -0600

    re-init panel, fix some naming

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,7 @@ namespace demoapp
         private void InitializeComponent()
         {
             this.SplitContainer = new System.Windows.Forms.SplitContainer();
-            this.DrawWallButton = new System.Windows.Forms.Button();
-            this.DrawFurnitureButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
-            this.SplitContainer.Panel1.SuspendLayout();
             this.SplitContainer.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,39 +43,13 @@ namespace demoapp
             this.SplitContainer.Location = new System.Drawing.Point(0, 0);
             this.SplitContainer.Name = "SplitContainer";
             // 
-            // SplitContainer.Panel1
-            // 
-            this.SplitContainer.Panel1.Controls.Add(this.DrawFurnitureButton);
-            this.SplitContainer.Panel1.Controls.Add(this.DrawWallButton);
-            // 
             // SplitContainer.Panel2
             // 
             this.SplitContainer.Panel2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.SplitContainer.Panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SplitContainer_Panel2_MouseDown);
             this.SplitContainer.Size = new System.Drawing.Size(1250, 577);
             this.SplitContainer.SplitterDistance = 625;
             this.SplitContainer.SplitterWidth = 1;
             this.SplitContainer.TabIndex = 0;
-            // 
-            // DrawWallButton
-            // 
-            this.DrawWallButton.Location = new System.Drawing.Point(495, 11);
-            this.DrawWallButton.Name = "DrawWallButton";
-            this.DrawWallButton.Size = new System.Drawing.Size(115, 29);
-            this.DrawWallButton.TabIndex = 0;
-            this.DrawWallButton.Text = "Draw Wall";
-            this.DrawWallButton.UseVisualStyleBackColor = true;
-            this.DrawWallButton.Click += new System.EventHandler(this.DrawWallButton_Click);
-            // 
-            // DrawFurnitureButton
-            // 
-            this.DrawFurnitureButton.Location = new System.Drawing.Point(495, 46);
-            this.DrawFurnitureButton.Name = "DrawFurnitureButton";
-            this.DrawFurnitureButton.Size = new System.Drawing.Size(115, 29);
-            this.DrawFurnitureButton.TabIndex = 1;
-            this.DrawFurnitureButton.Text = "Draw Furniture";
-            this.DrawFurnitureButton.UseVisualStyleBackColor = true;
-            this.DrawFurnitureButton.Click += new System.EventHandler(this.DrawFurnitureButton_Click);
             // 
             // Form1
             // 
@@ -90,7 +61,6 @@ namespace demoapp
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.SplitContainer.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
             this.SplitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -100,8 +70,6 @@ namespace demoapp
         #endregion
 
         private System.Windows.Forms.SplitContainer SplitContainer;
-        private System.Windows.Forms.Button DrawFurnitureButton;
-        private System.Windows.Forms.Button DrawWallButton;
     }
 }
 
