fe5c9fda01a22bece545a844a664ab6e5b41cc37
Michael Redman <50535591+mwr0007@users.noreply.github.com>
2022-02-04 14:36:31 -0600

    remove the rest of my demo stuff

--- Form1.cs
+++ Form1.cs
@@ -12,22 +12,9 @@ namespace demoapp
 {
     public partial class Form1 : Form
     {
-        private Brush ActiveBrush = Brushes.Red;
-        private Panel SimDrawingPanel;
-        private Graphics SimDrawingPanelGraphics;
-        private int PanelXBounds, PanelYBounds, PanelXMin, PanelYMin;
-        private static int SQUARE_SIZE = 25;
-        private static int GRID_DIM = 25;
-
         public Form1()
         {
             InitializeComponent();
-            SimDrawingPanel = SplitContainer.Panel2;
-            SimDrawingPanelGraphics = SimDrawingPanel.CreateGraphics();
-            PanelXBounds = SimDrawingPanel.Width;
-            PanelYBounds = SimDrawingPanel.Height;
-            PanelXMin = SimDrawingPanel.Width - PanelXBounds;
-            PanelYMin = SimDrawingPanel.Height - PanelYBounds;
         }
     }
 }
