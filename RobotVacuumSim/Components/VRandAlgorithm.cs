0d4e90f3d89b6fc1c53ce0f206a5d8533f52b630
Madelyn Wiggins <mrw0025@uah.edu>
2022-02-16 23:49:39 -0600

    Added missing component files

---
+++ RobotVacuumSim/Components/VRandAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacuumSim.Components
+{
+    /// <summary>
+    /// Subclass of VacuumController. Implements Random Pathing algorithm for the Vacuum.
+    /// </summary>
+    public class VRandAlgorithm : VacuumController
+    {
+        public override void ExecVPath()
+        {
+
+        }
+
+    }
+}
---
+++ RobotVacuumSim/Components/VSnakeAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacuumSim.Components
+{
+    /// <summary>
+    /// Subclass of VacuumController. Implements Snake Pathing algorithm for the Vacuum.
+    /// </summary>
+    public class VSnakeAlgorithm : VacuumController
+    {
+        public override void ExecVPath()
+        {
+
+        }
+
+    }
+}
