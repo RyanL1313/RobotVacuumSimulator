46586c1ab62c1ff75d1bc942684c871373aadfe7
RyanL1313 <ryn.lynch@gmail.com>
2022-04-02 01:26:37 -0500

    Include default floor plan as a resource. Write instructions for our software.

--- RobotVacuumSim/UI/Form1.cs
+++ RobotVacuumSim/UI/Form1.cs
@@ -257,7 +257,23 @@ namespace VacuumSim
 
         private void ShowInstructionsButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("To-do.\n\nsomething something vacuum go vroom vroom.");
+            MessageBox.Show("Welcome to the Robot Vacuum Simulator. Below is a short guide on how to use this software:\n\n" +
+                "The first step is to create a floor plan. You can either start from scratch or start from a pre-loaded floor plan.\n" + 
+                "There are two types of pre-loaded floor plans. A floor plan that you have created and saved, and a default floor plan\n" +
+                "created by us, the developers. You can then add/remove obstacles onto the floor plan. This can be achieved by selecting\n" +
+                "an obstacle (i.e. room, chair, table, chest) and then clicking-and-dragging the obstacle onto the floor plan. To remove obstacles,\n" +
+                "click on the \"Eraser Mode\" button to turn on eraser mode. Then, you can click on obstacles on the floor plan to remove them.\n" +
+                "Once you are satisfied with your floor plan, press \"Finish Floor Plan\". You can always go back to editing the floor plan by\n" + 
+                "pressing \"Edit Floor Plan\". Pressing \"Finish Floor Plan\" will allow you to start setting the vacuum's attributes.\n\n" +
+                "Customize the vacuum's attributes by utilizing the different options in the \"Vacuum Attributes\" section. Place the vacuum\n" +
+                "onto the floor plan by clicking-and-dragging the vacuum onto a valid position. Once the vacuum is placed, you can set the simulation\n" +
+                "speed and start the simulation. Note that if you choose to run all algorithms, four full simulations will run for each algorithm.\n\n" +
+                "Stop the simulation at any time by pressing \"Stop Simulation\". The simulation will automatically stop when the vacuum's battery runs out.\n" +
+                "As the simulation is running, a trail will be shown behind the vacuum showing what areas of the house have been cleaned so far by the vacuum.\n\n" +
+                "Once the simulation has ended, the results will get saved and can be reviewed at any time. Also, after the simulation has ended, a heat map will get displayed\n" +
+                "showing how well each area of the house was cleaned by the vacuum. The more red a spot is, the better the vacuum cleaned it. The more blue\n" +
+                "a spot is, the worse the vacuum cleaned it.\n\n" + 
+                "You can then proceed to run another simulation by pressing \"Yes\" when prompted.\n");
         }
 
         private void FinishOrEditFloorplanButton_Click(object sender, EventArgs e)
@@ -377,7 +393,7 @@ namespace VacuumSim
             HouseWidthSelector.Value = 50; // 25 tiles wide (excluding boundary walls)
             HouseHeightSelector.Value = 40; // 20 tiles high (excluding boundary walls)
 
-            FloorplanFileReader.LoadTileGridData("../../../UI/Floorplan/DefaultFloorPlan.txt", HouseLayout);
+            FloorplanFileReader.LoadTileGridData(Properties.Resources.DefaultFloorPlan.Split("\n"), HouseLayout);
 
             FloorCanvas.Invalidate(); // Re-trigger paint event
         }
