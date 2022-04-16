d3451ab2e0969103c2a7fb93a23132d436d14aa4
Michael Redman <50535591+mwr0007@users.noreply.github.com>
2022-04-16 16:34:32 -0500

    remove code re-use in FloorplanFileWriter

--- RobotVacuumSim/UI/Floorplan/FloorplanFileWriter.cs
+++ RobotVacuumSim/UI/Floorplan/FloorplanFileWriter.cs
@@ -15,7 +15,7 @@ namespace VacuumSim
         {
         }
 
-        public static void SaveTileGridData(string path, FloorplanLayout HouseLayout)
+        private static string[] GetTileGridData(FloorplanLayout HouseLayout)
         {
             string[] lines = new string[HouseLayout.numTilesPerCol + 1]; // NumTilesPerCol is technically the number of rows. Confusing, I know. The +1 is for the extra top row of data
 
@@ -30,25 +30,18 @@ namespace VacuumSim
                     lines[i + 1] = lines[i + 1] + " " + i + " " + j + " " + HouseLayout.floorLayout[j, i].obstacle + " " + HouseLayout.floorLayout[j, i].groupID;
                 }
             }
+            return lines;
+        }
 
+        public static void SaveTileGridData(string path, FloorplanLayout HouseLayout)
+        {
+            string[] lines = GetTileGridData(HouseLayout);
             File.WriteAllLines(path, lines);
         }
 
         public static string[] TileGridDataAsString(FloorplanLayout HouseLayout)
         {
-            string[] lines = new string[HouseLayout.numTilesPerCol + 1]; // NumTilesPerCol is technically the number of rows. Confusing, I know. The +1 is for the extra top row of data
-
-            lines[0] = "SavedGroupID " + currentObstacleGroupNumber + " "; // Save the obstacle group number we were on
-            lines[0] += "Dimensions " + HouseLayout.numTilesPerRow + " " + HouseLayout.numTilesPerCol; // Save the floorplan dimensions
-
-            for (int i = 0; i < HouseLayout.numTilesPerCol; i++)
-            {
-                lines[i + 1] = i + " " + "0" + " " + HouseLayout.floorLayout[0, i].obstacle + " " + HouseLayout.floorLayout[0, i].groupID; // First value in row
-                for (int j = 1; j < HouseLayout.numTilesPerRow; j++)
-                {
-                    lines[i + 1] = lines[i + 1] + " " + i + " " + j + " " + HouseLayout.floorLayout[j, i].obstacle + " " + HouseLayout.floorLayout[j, i].groupID;
-                }
-            }
+            string[] lines = GetTileGridData(HouseLayout);
 
             return lines;
         }
